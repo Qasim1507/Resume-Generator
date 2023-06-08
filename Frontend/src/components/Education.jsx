96c0991d5c64b812721c3d21a08991604cb81179
nalawalaq <nalawalaq@gmail.com>
2023-06-08 23:31:28 +0530

    education input

--- Frontend/src/components/Education.jsx
+++ Frontend/src/components/Education.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import Input from "./input";
+import "../public/styles.css"
 
 function Education() {
   return (
     <div>
       <form className="form">
-        <legend>Education Details</legend>
-        <label for="institute">Institute</label><br/>
-        <Input type="text" id="institute" placeholder="Institute" />
-        <label for="degree">Degree</label><br/>
+        <h1>Education Details</h1>
+        <h2><label for="level">Level of Studies</label></h2><br/>
+        <Input type="text" id="level" placeholder="Level" />
+        <h2><label for="degree">Degree</label></h2><br/>
         <Input type="text" id="degree" placeholder="Degree" />
-        <label for="marks">Percentage</label><br/>
+        <h2><label for="institute">Institute</label></h2><br/>
+        <Input type="text" id="institute" placeholder="Institute" />
+        <h2><label for="city">City</label></h2><br/>
+        <Input type="text" id="city" placeholder="City" />
+        <h2><label for="marks">Percentage</label></h2><br/>
         <Input type="text" id="marks" placeholder="Percentage" />
-        <label for="yop">Year of Passing</label><br/>
+        <h2><label for="yop">Year of Passing</label></h2><br/>
         <Input type="date" id="yop" placeholder="Year of Passing" />
         <button type="submit" value="submit">Next Page</button>
       </form>
